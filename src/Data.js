63c3d715f830873fe000bcbb548b0ad898410ab9
Parashar Neupane <parasharneupane3900@gmail.com>
2023-10-01 17:02:47 +0545

    Update Project

--- src/Data.js
+++ src/Data.js
@@ -3,16 +3,16 @@ export const ProjectData = [
         id: 1, title: "Pharmacy Management System", url: "./img/pms.png", desc: "Used HTML,CSS,JS,PHP and Bootstrap.Inventory Management,User Management,Admin and User Panel,Leave Management are the features.", git: "https://github.com/unique3900/Pharmacy-Management-System"
     },
     {
-        id: 2, title: "Ecommerce Website", url: "./img/ecm.png", desc: "React JS,Node JS,Mongo DB,Express Js,Tailwind.Admin and User panel,Pagination,Category & Price Filter,State management,Payment gateway integration,Json Web Token for authentication are some features.", git: "https://github.com/unique3900/Ecommerce-Project-MERN-Stack-6th-Sem"
+        id: 2, title: "Ecommerce Website", url: "./img/ecm.png", desc: "React JS,Node JS,Mongo DB,Express Js,Tailwind. Admin and User panel,Pagination,Category & Price Filter,State management,Payment gateway integration,Json Web Token for authentication are some features. I am currently working on this project.", git: "https://github.com/unique3900/Ecommerce-Project-MERN-Stack-6th-Sem"
     },
     {
-        id: 3, title: "Blogging Website", url: "./img/blog.png", desc: "React JS,Node JS,MYSQL,Express Js,Tailwindm,Redux.New Post,Search Post,Edit Post,Delete Post,JWT for user authentication are some features.", git: "https://github.com/unique3900/Full-Stack-Blog-System-React-Node-js-Express-js-and-MYSQL"
+        id: 3, title: "Blogging Website", url: "./img/blog.png", desc: "React JS,Node JS,MYSQL,Express Js,Tailwind,Redux. New Post,Search Post,Edit Post,Delete Post,JWT for user authentication are some features.", git: "https://github.com/unique3900/Full-Stack-Blog-System-React-Node-js-Express-js-and-MYSQL"
     },
     {
-        id: 4, title: "Hotel Booking Website", url: "./img/hotel.png", desc: "React JS,Node JS,Mongo DB,Express Js,Tailwindm,Context API.I am currently working on this project will update soon.", git: "https://github.com/unique3900/Hotel-Booking-System"
+        id: 4, title: "Hotel Booking Website", url: "./img/hotel.png", desc: "React JS,Node JS,Mongo DB,Express Js,Tailwind,Context API.I am currently working on this project will update soon.", git: "https://github.com/unique3900/Hotel-Booking-System"
     },
     {
-        id: 5, title: "Wil Update Soon", url: "./img/coming.avif", desc: "Will Update with new project after the completion of the project,Thank You!", git: "https://github.com/unique3900"
+        id: 5, title: "ChillFlix-Movie Tracker", url: "./img/chillflix.png", desc: "React,Tailwind,Firebase. Allows user to create their watchlist and also firebase authentication is used. TMDB API is used.", git: "https://github.com/unique3900/ChillFlix"
     },
     {
         id: 6, title: "Wil Update Soon", url: "./img/coming.avif", desc: "Will Update with new project after the completion of the project,Thank You!", git: "https://github.com/unique3900"
