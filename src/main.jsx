8fa931747a03241af3fc6a0fa4d3ece1206eb651
parashar <parasharneupane3900@gmail.com>
2023-10-31 11:35:37 +0545

    Setup2 For Tag Manager

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import TagManager from 'react-gtm-module'
 import './index.css'
 
+const tagManagerArgs = {
+  gtmId: 'GTM-NP7N542V'
+}
+
+
+TagManager.initialize(tagManagerArgs)
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
